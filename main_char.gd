08f9a288b88b4b044709cfb063395433f590b70c
whysixmift <miftasigma11@gmail.com>
2025-09-27 14:37:51 +0700

    added few things

--- main_char.gd
+++ main_char.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var sprite_2d = $Sprite2D
+# Reference to the 'debu' particle node
+@onready var debu_particles = $debu
 
 func _physics_process(delta: float) -> void:
 	#animations
@@ -18,6 +20,10 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		# Emit 'debu' particles on jump
+		if debu_particles:
+			debu_particles.emitting = false # Reset in case still emitting
+			debu_particles.emitting = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
